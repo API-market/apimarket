6d6be20eeabbd4ba81ffd16b4235bf3d5375cd7e
Tyler Kuhn <tyler@wargoats.com>
2018-03-15 14:11:56 -0700

    clients should be able to use the client lib with a minimal amount of effort

--- index.js
+++
@@ -1,8 +0,0 @@
-const client = require('lib/client')
-const server = require('lib/server')
-
-// this function is exposed so API providers can build their own server, injecting this middleware as appropriate
-exports.middlewareFor = server.middlewareFor
-
-// returns an express server ready to `listen` on a `port`
-exports.initServer = server.build
