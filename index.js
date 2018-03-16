f43a8781919df27dbb71b3d964cc53e54056be3c
Tray Lewin <tray@aikon.com>
2018-03-16 15:29:09 -0700

    Add asyncHandler

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const client = require('./lib/client')
 const server = require('./lib/server')
+const asyncHandler = require('express-async-handler')
 
 // this function is exposed so API providers can build their own server, injecting this middleware as appropriate
-exports.middlewareFor = server.middlewareFor
+exports.middlewareFor = asyncHandler(server.middlewareFor)
 
 // returns an express server ready to `listen` on a `port`
 exports.initServer = server.build
