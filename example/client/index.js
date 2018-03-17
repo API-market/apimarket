17fc2f9d7d7012c4adebf489d5ed87a08c4cd1be
Alex Stokes <r.alex.stokes@gmail.com>
2018-03-16 17:53:53 -0700

    Parse public key properly

--- example/client/index.js
+++ example/client/index.js
@@ -2,18 +2,17 @@ const fs = require('fs')
 const aikon = require('../../index');
 
 const configFilePath = '/config.json'
+const config = JSON.parse(fs.readFileSync(__dirname + configFilePath))
 
 const registrySelector = 'io.hadron.spaceTelescope'
+const request = {"query":"{ DeepSpace(image:\"https://cdn.spacetelescope.org/archives/images/thumb300y/potw1452a.jpg\") {results} }"}
 
 const run = async () => {
-  const config = JSON.parse(fs.readFileSync(__dirname + configFilePath))
-
   let client = await aikon.init(config)
 
   try {
-    const response = await client.fetch(registrySelector, {x: 1})
-
-    console.log("RESULT:", response)
+    const response = await client.fetch(registrySelector, request)
+    console.log(JSON.stringify(response, null, 2))
   } catch(err) {
     console.error(err)
   }
