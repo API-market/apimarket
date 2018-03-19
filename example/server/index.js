68a24403229db91d397107b3206cec8dff202d5d
Alex Stokes <r.alex.stokes@gmail.com>
2018-03-19 16:39:57 -0700

    move config to file for uniformity

--- example/server/index.js
+++ example/server/index.js
@@ -1,29 +1,9 @@
 const { initServer } = require('../../index')
 
-const config = {
-  "registry": {
-    "io.hadron.spaceTelescope": {
-      "endpoint": 'http://sandbox.dev.aikon.com:3405/',
-      "offer": {
-        "address": '0x19f9591fbe24b596434aaad7c80884a863a56b0e'
-      }
-    }
-  },
-  "verifier": {
-    "publicKey": `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEfy0YUA2nDgEIbOC2FxE6wygiNFUd
-eH6vYfcHz+uioYq+GU81Axysh09GKhxsRu4OIK668BzCyw9DI/HDysTV2A==
------END PUBLIC KEY-----`
-  },
-  "services": {
-    "web3": {
-      "endpoint": "http://ganache1.api.market:8545"
-    },
-    "ipfs": {
-      "endpoint": "http://ipfs2-ext.dev.api.market:5001"
-    }
-  }
-}
+const fs = require('fs')
+
+const configFilePath = '/config.json'
+const config = JSON.parse(fs.readFileSync(__dirname + configFilePath))
 
 const port = process.env.PORT || 8080
 
