6b8817c19e372ea9645b33ac26767c0969a83988
Surabhi Lodha <surabhil@usc.edu>
2018-08-22 17:50:48 -0700

    separate server modules. Export middleware as well as checkOreAccessToken function

--- server/lib/server.js
+++ server/lib/server.js
@@ -1,77 +1,98 @@
-require('newrelic')
 require('dotenv').config()
 const analyticsEvent = require(__dirname + "/segment")
 const ecc = require('eosjs-ecc')
 const jwt = require('jsonwebtoken')
 const sortJson = require('sort-json')
-const { logError } = require('./logging')
+const {
+  logError
+} = require('./logging')
 
-function checkHash() {
-    return async (req, res, next) => {
-        let requestParams
-        // Check if the hash of the request parameters matches the hash included in the ore access token issued by the verifier
-        if(JSON.stringify(req.query) === JSON.stringify({})){
-          requestParams = req.body
-        } else{
-          requestParams = req.query
-        }
-        
-      const reqParamHash = req.reqParamHash
-      try {
-        const sortedReqParams = sortJson(requestParams)
-    
-        const hash = ecc.sha256(JSON.stringify(sortedReqParams))
-
-        if(hash === reqParamHash){
-          next()
-        } else {
-          throw e
-        }
-      } catch (e) {
-        return res.status(401).json({message: "the request parameters sent to the api server are differnet from thise sent to the verifier"})
-      }
+function checkRequestParams(reqParamHash, requestParams) {
+  // Check if the hash of the request parameters matches the hash included in the ore access token issued by the verifier
+  try {
+    const sortedReqParams = sortJson(requestParams)
+    const hash = ecc.sha256(JSON.stringify(sortedReqParams))
+    if (hash === reqParamHash) {
+      return true
+    } else {
+      return false
     }
+  } catch (error) {
+    throw new Error(`${errMsg} ${error}`);
   }
+}
 
+async function checkOreAccessToken(oreAccessToken, requestParams) {
+  try {
+    let errMsg;
+
+    if (!process.env.VERIFIER_PUBLIC_KEY) {
+      errMsg = `verifier public key is missing. Provide a valid verifier public key`;
+      throw new Error(`${errMsg}`);
+    }
+
+    const verifierPublicKey = process.env.VERIFIER_PUBLIC_KEY.replace(/\\n/g, '\n')
+    // verify ore access token is a valid jwt token signed by the client
+    const payload = await jwt.verify(oreAccessToken, verifierPublicKey, {
+      algorithms: ["ES256"]
+    })
+    return checkRequestParams(payload.reqParamHash, requestParams)
+  } catch (error) {
+    errHead = `invalid ore-access-token`
+    if (error.message == 'jwt expired') {
+      errMsg = `Expired ore-access-token. Provide a valid token.`
+    }
+    if (error.message == 'invalid signature') {
+      errMsg = `invalid signature for ore-access-token. Make sure ore-access-token is signed with a valid key`
+    }
+    if (error.message == 'jwt malformed') {
+      errMsg = `Malformed ore-access-token. Make sure the ore-access-token has the valid right name and voucher.`
+    }
+    logError("Error", `${errHead}:${error.message}`)
+    return false
+  }
+}
+
+//export as a middleware
 function apiMarketRequestValidator() {
-    return async (req, res, next) => {     
-    try{
-      if(!process.env.VERIFIER_PUBLIC_KEY){
-        return res.status(401).json({message:"verifier public key not found. Pass in verifier public key as an"})
+  return async (req, res, next) => {
+    try {
+      if (!process.env.VERIFIER_PUBLIC_KEY) {
+        return res.status(400).json({
+          message: "verifier public key not found. Pass in verifier public key as an environment variable."
+        })
       }
       // Check if access token exists
-      if(req.headers['ore-access-token']){
-        const accessToken = await req.headers['ore-access-token']
-        const accessTokenHash = ecc.sha256(JSON.stringify(accessToken))
-        const ip = req.connection.remoteAddress || req.headers['x-forwarded-for']
-        const verifierPublicKey = process.env.VERIFIER_PUBLIC_KEY.replace(/\\n/g, '\n')
-        const payload = jwt.verify(accessToken, verifierPublicKey, {
-            algorithms: ["ES256"]
+      if (req.headers['ore-access-token']) {
+        let requestParams = Object.assign(req.query, req.body)
+        let ifValid = await checkOreAccessToken(req.headers['ore-access-token'], requestParams)
+
+        if (ifValid) {
+          // record data in segment 
+          const ip = req.connection.remoteAddress || req.headers['x-forwarded-for']
+          const accessTokenHash = ecc.sha256(JSON.stringify(req.headers['ore-access-token']))
+          analyticsEvent(ip, "request details", {
+            accessTokenHash
           })
-        
-      // Check if the access token is valid
-        if(payload){
-            req.reqParamHash = payload["reqParamHash"]
-            checkHash()
-            next()
+          // return the control back to next middleware
+          next()
         } else {
-            return res.status(401).json({message:"invalid api market access token"})
+          return res.status(401).json({
+            message: "the ore access token is not valid"
+          })
         }
-        analyticsEvent(ip,"request details", {accessTokenHash})
+      } else {
+        return res.status(400).json({
+          message: "ore access token not found in the request. Pass in a valid ore access token."
+        })
       }
-      else{
-        return res.status(401).json({message:"api market access token not found"})
-      
-      }   
-    }
-    catch (e) {
-      logError("Error", e)
+    } catch (error) {
+      logError("Error", error)
     }
   }
 }
-  
+
 module.exports = {
-  apiMarketRequestValidator
+  apiMarketRequestValidator,
+  checkOreAccessToken
 }
-
-
