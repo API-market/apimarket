4fcfc4e3a5684816ee399ddd92ef0187c5ff0f4d
Tray Lewin <tray.lewin@live.com>
2018-11-25 13:37:24 -0800

    move rollbar and segment settings to .env

--- server/src/rollbar.js
+++ server/src/rollbar.js
@@ -1,13 +1,30 @@
+
+/*
+    Rollbar is a 3rd party incident reporting tool. 
+    To enable errors or crashes to be posted to it, register for an account at rollbar.com then create an access key
+    then base64 encode the key using a command like...
+    ... 'base64 yourkey.file | pbcopy' - on a Mac, this copies the base64 encoded value to the clipboard
+    Set the value of ROLLBAR_KEY_BASE64_ENCODED in your .env file
+*/
+
+require('dotenv').config()
 const base64 = require('base-64')
 var Rollbar = require('rollbar');
 
-const rollbarConfig = {
-    accessToken: base64.decode("MTFiNGU0MDY3M2M0NDk4MjkzMmFkODI3ODc3ZTNjYWQ="), 
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    payload: {
-        environment: "development"
-    }
-};
+var rollbarExport = () => {};
 
-module.exports = rollbar = new Rollbar(rollbarConfig);
+const rollbarKeyEncoded = process.env.ROLLBAR_KEY_BASE64_ENCODED;
+if(rollbarKeyEncoded) {
+    try {
+        const rollbarConfig = {
+            accessToken: base64.decode(rollbarKeyEncoded), 
+            captureUncaught: true,
+            captureUnhandledRejections: true
+        };
+        rollbarExport = new Rollbar(rollbarConfig);
+    } catch (error) {
+        console.log(`Problem initializing Rollbar: Check ROLLBAR_KEY_BASE64_ENCODED config setting`);
+    }
+}
+
+module.exports = rollbar = rollbarExport;
--- server/src/segment.js
+++ server/src/segment.js
@@ -1,15 +1,33 @@
-require('dotenv')
-const Analytics = require('analytics-node')
-const base64 = require('base-64')
-const segmentKey = base64.decode("aThDVFVJNEJYRjlnWlVrZUhuUjJkYmR6S3JXY2E3SHM")
-const analytics = new Analytics(segmentKey)
-const { log } = require('./logging')
+/*
+    Segment is a 3rd party analytics tool 
+    To enable logging of usage of this service, register for an account at segment.com then create an access key
+    Set the value of SEGMENT_KEY in your .env file
+*/
+
+require('dotenv').config()
+const Analytics = require('analytics-node');
+const base64 = require('base-64');
+const { log } = require('./logging');
+
+var analytics;
+
+const segmentKey = process.env.SEGMENT_KEY;
+if(segmentKey) {
+  try {
+    analytics = new Analytics(segmentKey);
+  } catch (error) {
+    console.log(`Problem initializing Segment: Check SEGMENT_KEY config setting`);
+  }
+}
 
 module.exports = function analyticsEvent(userId, eventName, eventMetadata={}) {  
     log(eventName, eventMetadata)
-    analytics.track({
-      event: eventName,
-      properties: eventMetadata,
-      userId: userId
-    });
-  }
+    if(analytics) {
+      analytics.track({
+        event: eventName,
+        properties: eventMetadata,
+        userId: userId
+      });
+    }
+}
+
